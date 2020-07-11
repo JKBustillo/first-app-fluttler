6e99b6a4dc2ce7df4cb47bc49a578d26959c2807
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-10 22:56:19 -0500

    feat: simple use of a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(new MaterialApp(
-    home: MyApp(),
+    home: MyButton(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyButton extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    final double iconSize = 40.0;
-    final TextStyle textStyle = new TextStyle(
-      color: Colors.grey,
-      fontSize: 30.0
-    );
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Stateless Widget")
-      ),
-      body: new Container(
-        child: Center(
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              new MyCard(title: new Text("I Love Flutter", style: textStyle), icon: new Icon(Icons.favorite, size: iconSize, color: Colors.red)),
-              new MyCard(title: new Text("I Like Development", style: textStyle), icon: new Icon(Icons.computer, size: iconSize)),
-              new MyCard(title: new Text("Next Video", style: textStyle), icon: new Icon(Icons.queue_play_next, size: iconSize, color: Colors.purple))
-            ],
-          ),
-        )
-      ),
-    );
-  }
+  _MyButtonState createState() => new _MyButtonState();
 }
 
-class MyCard extends StatelessWidget {
-  final Widget title;
-  final Widget icon;
+class _MyButtonState extends State<MyButton> {
+  String text = "";
 
-  MyCard({this.title, this.icon});
-
+  void onPressButton() {
+    setState(() {
+      text = new Random().nextInt(100).toString();
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 2.0),
-      child: new Card(
-        child: new Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Column (
-            children: <Widget>[this.title, this.icon],
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Stateful Widget"),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: new Container(
+        child: new Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(text, style: new TextStyle(fontSize: 50.0)),
+              new Padding(padding: new EdgeInsets.all(10.0)),
+              new RaisedButton(
+                child: new Text("Update", style: new TextStyle(color: Colors.white)),
+                color: Colors.blueAccent,
+                onPressed: onPressButton,
+              )
+            ],
           ),
         )
       ),
