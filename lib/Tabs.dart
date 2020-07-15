4ddcfdcba88b49725ed7f95ea0583b972ad5c925
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-15 18:22:53 -0500

    feat: tabs imported from pages

---
+++ lib/Tabs.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/pages/home.dart';
+import 'package:first_app/pages/video.dart';
+import 'package:first_app/pages/contact.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: MyTabs(),
+  ));
+}
+
+class MyTabs extends StatefulWidget {
+  MyTabs({Key key}) : super(key: key);
+
+  @override
+  _MyTabsState createState() => _MyTabsState();
+}
+
+class _MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
+  TabController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: new AppBar(
+         title: new Text("My Tabs"),
+         backgroundColor: Colors.redAccent,
+         bottom: new TabBar(
+           tabs: <Widget>[
+             new Tab(
+               icon: new Icon(Icons.home),
+             ),
+             new Tab(
+               icon: new Icon(Icons.video_library),
+             ),
+             new Tab(
+               icon: new Icon(Icons.contacts),
+             )
+           ],
+           controller: controller,
+         ),
+       ),
+       body: new TabBarView(
+         children: <Widget>[
+           new Home(), new Video(), new Contact()
+         ],
+         controller: controller,
+       ),
+    );
+  }
+}
