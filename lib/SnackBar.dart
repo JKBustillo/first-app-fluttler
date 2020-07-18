9e73f743caca2a7b656d6285fb223b0f02f7766e
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-18 15:21:55 -0500

    feat: snackbar example

---
+++ lib/SnackBar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: MySnackBar(),
+  ));
+}
+
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("SnackBar"),
+      ),
+      body: new Center(
+        child: new MyButton(),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: new Text("Press me"),
+      onPressed: () {
+        Scaffold.of(context).showSnackBar(new SnackBar(
+          content: new Text("Couldn't you help it, huh?"),
+          duration: new Duration(seconds: 5),
+          action: new SnackBarAction(
+            label: "Press here",
+            onPressed: () {
+              Scaffold.of(context).showSnackBar(new SnackBar(
+                content: new Text("You did it again!"),
+                duration: new Duration(seconds: 5),
+              ));
+            }
+          ),
+        ));
+      },
+    );
+  }
+}
