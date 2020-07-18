1f052ec8ea0f792c0f57ceda99ea8e6ac3f1601c
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-17 20:08:11 -0500

    feat: SL widget with a button in the middle

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,27 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(new MaterialApp(
-    home: MyNavigator(),
+    home: MyFirst(),
   ));
 }
 
-class MyNavigator extends StatefulWidget {
-  MyNavigator({Key key}) : super(key: key);
-
-  @override
-  _MyNavigatorState createState() => _MyNavigatorState();
-}
-
-class _MyNavigatorState extends State<MyNavigator> with SingleTickerProviderStateMixin {
-  TabController controller;
-  @override
-  void initState() {
-    super.initState();
-    controller = new TabController(length: 3, vsync: this);
-  }
+class MyFirst extends StatelessWidget {
+  const MyFirst({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("Bottom Navigation Bar"),
+        title: new Text("First Screen")
       ),
-      bottomNavigationBar: new Material(
-        color: Colors.blueAccent,
-        child: new TabBar(
-          tabs: <Tab> [
-            new Tab(
-              icon: new Icon(Icons.alarm),
-            ),new Tab(
-              icon: new Icon(Icons.watch_later),
-            ),new Tab(
-              icon: new Icon(Icons.airplanemode_active),
-            )
-          ],
-          controller: controller,
-        ),
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text("Show new screen"),
+          onPressed: () {
+
+          },
+        )
       ),
     );
   }
