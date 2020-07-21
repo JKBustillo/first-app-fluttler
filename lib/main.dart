a6f4a29a620db54e00be6f13f5702dc1a2d5a1a3
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-20 22:28:54 -0500

    feat: appBar ready and english_words package imported

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/contact/contact.dart';
-
+import 'package:english_words/english_words.dart';
 void main() {
   runApp(new MaterialApp(
-    home: new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Contacts")
-      ),
-      body: new Contact(),
-    ),
+    home: MyRandomWords(),
   ));
+}
+
+class MyRandomWords extends StatefulWidget {
+
+  @override
+  _MyRandomWordsState createState() => _MyRandomWordsState();
+}
+
+class _MyRandomWordsState extends State<MyRandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: new AppBar(
+         title: new Text("Infinite List"),
+         centerTitle: true,
+       ),
+    );
+  }
 }
